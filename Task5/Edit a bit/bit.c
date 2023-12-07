8a7402eec56c1855d7631a54661df0460006c1f4
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2023-12-08 01:39:12 +0200

    Update bit.c

--- Task5/Edit a bit/bit.c
+++ Task5/Edit a bit/bit.c
@@ -1,30 +1,52 @@
+
 #include <stdio.h>
 
 int Set_Num (int num , int bit){
- int i= num & (1<<bit);
+ int i= num | (1<<bit);
  return i;
 }
 
 
+int Clear_Num (int num , int bit){
+  int i= num | (~(1<<bit)) ;
+  return i;
+}
+
+
+int Toggle_Num (int num , int bit){
+   int i= num ^ (1<<bit) ;
+   return i; 
+}
+
+
+int Read_Num (int num , int bit){
+    int i= (num>>bit)&1;
+    return i;
+}
 
 int main(){
     int nnum,bbit;
-    char operator;
+     int operator;
     printf("Enter the number:");
     scanf("%d",&nnum);
     printf("Enter the number of the bit:");
     scanf("%d",&bbit);
-    printf("Enter the operator");
-    scanf("%c",&operator);
+    printf("Enter the operator:");
+    scanf(" %d",&operator);
     switch (operator){
-     case 's' :
+     case 1 :
       printf("%d" , Set_Num(nnum,bbit));
       break;
+     case 2 :
+      printf("%d" , Clear_Num(nnum,bbit));
+      break;
+     case 3 :
+      printf("%d" , Toggle_Num(nnum,bbit));
+      break;
+     case 4 :
+      printf("%d" , Read_Num(nnum,bbit));
+      break;
+      
     }
-
-
-
-
-
     return 0;
 }
