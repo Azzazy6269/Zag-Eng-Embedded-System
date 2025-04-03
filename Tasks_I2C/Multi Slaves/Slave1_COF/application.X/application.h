f321343543fdf22f23a3a1a66aab956959847397
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2025-04-03 04:10:06 +0200

    Add files via upload

---
+++ Tasks_I2C/Multi Slaves/Slave1_COF/application.X/application.h
@@ -0,0 +1,44 @@
+/* 
+ * File:   application.h
+ * Author: moham
+ *
+ * Created on April 22, 2024, 6:23 PM
+ */
+
+#ifndef APPLICATION_H
+#define	APPLICATION_H
+
+/* Section : Includes */
+#include "MCAL_Layer/mcal_std_types.h"
+#include "MCAL_Layer/device_config.h"
+#include "ECU_Layer/ECU_layer_intialize.h"
+#include "MCAL_Layer/Interrupt/External_interrupt.h"
+#include "MCAL_Layer/EEPROM/hal_EEPROM.h"
+#include "MCAL_Layer/ADC/adc.h"
+#include "MCAL_Layer/Timer0/timer0.h"
+#include "MCAL_Layer/Timer1/Timer1.h"
+#include "MCAL_Layer/Timer2/Timer2.h"
+#include "MCAL_Layer/Timer3/Timer3.h"
+#include "MCAL_Layer/CCP1/CCP1.h"
+#include "MCAL_Layer/USART/usart.h"
+#include "MCAL_Layer/I2C/I2C.h"
+
+/* Section : MACRO Declerations */
+
+
+/* Section : MACRO Functions Declerations */
+
+
+/* Section : Data Types Declerations */
+
+/* Section : Function Declerations */
+void EUSART_TxDefaultInterruptHandler(void);
+void EUSART_RxDefaultInterruptHandler(void);
+void EUSART_framingErrorDefaultInterruptHandler(void);
+void EUSART_OverrunErrorDefaultInterruptHandler(void);
+
+
+
+
+#endif	/* APPLICATION_H */
+
