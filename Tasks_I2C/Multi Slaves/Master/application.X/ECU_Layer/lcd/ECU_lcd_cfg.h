d7ec7a6b6122c9b1d1b1915fa4310b41652bfb95
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2025-04-03 04:07:35 +0200

    Add files via upload

---
+++ Tasks_I2C/Multi Slaves/Master/application.X/ECU_Layer/lcd/ECU_lcd_cfg.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   ECU_LCD_CFG.h
+ * Author: moham
+ *
+ * Created on June 24, 2024, 1:15 PM
+ */
+
+#ifndef ECU_LCD_CFG_H
+#define	ECU_LCD_CFG_H
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
